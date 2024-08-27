cbe5c8917c2004eca39d09ab693208887b70e660
Hao Nguyen <haonguyen03.work@gmail.com>
2024-08-27 19:10:40 +0700

    adjust better translation for title and meta description

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation();
+  useEffect(() => {
+    document.title = t('page_title'); 
+  }, [i18n.language, t]); 
+  useEffect(() => {
+    document.querySelector('meta[name="description"]').setAttribute('content', t('meta_description'));
+  }, [i18n.language, t]);
   return (
     <Router>
       <Routes>
